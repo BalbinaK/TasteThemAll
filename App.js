3945818e3223c6995741ec529450b896da7e1933
BalbinaK <balbuhhha@gmail.com>
2020-01-29 15:47:35 +0100

    add react navigation

--- App.js
+++ App.js
@@ -1,49 +1,13 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  Image,
-} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <View style={styles.scrollView}>
-          <View style={styles.main}>
-            <View style={styles.image}>
-              <Image
-                source={require('./assets/intro_beer_icon.png')}
-                style={{height: 100, width: 100}}
-              />
-            </View>
-            <Text style={styles.title}>Taste Them All</Text>
-          </View>
-        </View>
-      </SafeAreaView>
-    </>
-  );
-};
+import HomeScreen from './src/HomeScreen';
 
-const styles = StyleSheet.create({
-  scrollView: {height: '100%'},
-  main: {
-    height: '100%',
-    backgroundColor: '#ddd',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 20,
-  },
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: HomeScreen},
 });
+
+const App = createAppContainer(MainNavigator);
 
 export default App;
---
+++ src/HomeScreen.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+export default class HomeScreen extends Component {
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <SafeAreaView>
+        <View style={styles.scrollView}>
+          <View style={styles.main}>
+            <TouchableOpacity
+              style={{alignItems: 'center'}}
+              onPress={() => navigate('Profile', {name: 'Jane'})}>
+              <View style={styles.image}>
+                <Image
+                  source={require('../assets/intro_beer_icon.png')}
+                  style={{height: 100, width: 100}}
+                />
+              </View>
+              <Text style={styles.title}>Taste Them All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {height: '100%'},
+  main: {
+    height: '100%',
+    backgroundColor: '#ddd',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 20,
+  },
+});
