d7f7d47a0bcb5c5d4209eb2f82e0558b95b27a1e
BalbinaK <balbuhhha@gmail.com>
2020-01-29 15:25:11 +0100

    update dependencies and fix eslint error

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
   Image,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="light-content" />
